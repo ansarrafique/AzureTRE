9e4e9085b9d364114e6aaa6f0f28f8b7654a2e64
Paul Demanze <keiran1337@gmail.com>
2026-04-10 13:35:35 +0100

    temporary fix

--- templates/workspace_services/azuresql/terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "azuresql_fqdn" {
-  value = azurerm_mssql_server.azuresql.fully_qualified_domain_name
-}
--- templates/workspace_services/openai/terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "openai_fqdn" {
-  value = azurerm_cognitive_account.openai.endpoint
-}
-
-output "openai_deployment_id" {
-  value = azurerm_cognitive_deployment.openai.name
-}
