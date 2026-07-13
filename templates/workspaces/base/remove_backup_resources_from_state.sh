78e8a2e1e3a4041921edef57847b47aa707e54d4
Ansar Rafique <Ansark6@gmail.com>
2026-07-13 20:21:17 +0100

    Improve code and script that removes storage account and recovery vault from terraform state file and bump version to 2.84.4

--- templates/workspaces/base/remove_backup_resources_from_state.sh
+++ templates/workspaces/base/remove_backup_resources_from_state.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
+
 set -o errexit
 set -o pipefail
+# Uncomment this line to see each command for debugging (careful: this will show secrets!)
+# set -o xtrace
 
 function usage() {
     cat <<USAGE
-Usage: $0 --backup_enabled true|false --use_azuread_auth true|false --use_oidc true|false --resource_group_name rg_name --storage_account_name sa_name --container_name container --key backend_key
-Options:
-    --backup_enabled         Whether backup is enabled (true|false)
-    --use_azuread_auth       Whether to use Azure AD auth (true|false)
-    --use_oidc               Whether to use OIDC (true|false)
-    --resource_group_name    Backend resource group name
-    --storage_account_name   Backend storage account name
-    --container_name         Terraform state container name
-    --key                    Backend key for terraform state
+    Usage: $0 --backup_enabled true|false --use_azuread_auth true|false --resource_group_name rg_name --storage_account_name sa_name --container_name container --key backend_key
+    Options:
+        --backup_enabled              Whether backup is enabled (true|false)  
+        --use_azuread_auth           Whether to use Azure AD auth (true|false)
+        --use_oidc                   Whether to use OIDC (true|false)
+        --resource_group_name        Backend resource group name
+        --storage_account_name       Backend storage account name  
+        --container_name             Terraform state container name
+        --key                        Backend key for terraform state
 USAGE
     exit 1
 }
 
+
+# if no arguments are provided, return usage function
 if [ $# -eq 0 ]; then
-    usage
+    usage # run usage function
 fi
+
+
 
 while [ "$1" != "" ]; do
     case $1 in
@@ -30,10 +37,6 @@ while [ "$1" != "" ]; do
     --use_azuread_auth)
         shift
         use_azuread_auth=$1
-        ;;
-    --use_oidc)
-        shift
-        use_oidc=$1
         ;;
     --resource_group_name)
         shift
@@ -57,60 +60,54 @@ while [ "$1" != "" ]; do
         ;;
     esac
 
-    if [[ -z "${2:-}" ]]; then
-        break
+    if [[ -z "$2" ]]; then
+      # if no more args then stop processing
+      break
     fi
 
-    shift
+    shift # remove the current value for `$1` and use the next
 done
 
+# done with processing args and can set this
 set -o nounset
 
 cd terraform/
 terraform version
+
+echo "${backend_rg_name}"
+echo "${backend_storage_account_name}"
+echo "${terraform_state_container_name}"
+echo "${backend_key}"
+echo "${use_azuread_auth}"
 
 terraform init -input=false -backend=true -reconfigure \
     -backend-config="resource_group_name=${backend_rg_name}" \
     -backend-config="storage_account_name=${backend_storage_account_name}" \
     -backend-config="container_name=${terraform_state_container_name}" \
     -backend-config="key=${backend_key}" \
-    -backend-config="use_azuread_auth=${use_azuread_auth}" \
-    -backend-config="use_oidc=${use_oidc}"
+    -backend-config="use_azuread_auth=${use_azuread_auth}"
 
-state_list=$(terraform state list || true)
+state_list=$(terraform state list)
 
 resources=(
     "azurerm_resource_group.ws"
     "azurerm_storage_account.stg"
     "azapi_resource.shared_storage"
     "azurerm_storage_account_network_rules.stgrules"
-    "azurerm_storage_container.stgcontainer"
-    "azurerm_private_endpoint.stgfilepe"
-    "azurerm_private_endpoint.stgblobpe"
-    "azurerm_private_endpoint.stgdfspe"
+    "azurerm_key_vault.kv"
 )
 
 for resource in "${resources[@]}"; do
     if echo "$state_list" | grep -Fxq "$resource"; then
-        echo "Removing ${resource} from state"
         terraform state rm "$resource"
     fi
 done
 
 if [[ "${backup_enabled}" == "true" ]]; then
-    backup_resources=(
-        "module.backup[0].azurerm_recovery_services_vault.vault"
-        "module.backup[0].azurerm_backup_container_storage_account.storage_account"
-        "module.backup[0].azurerm_backup_protected_file_share.file_share"
-    )
-
-    for resource in "${backup_resources[@]}"; do
-        if echo "$state_list" | grep -Fxq "$resource"; then
-            echo "Removing ${resource} from state"
-            terraform state rm "$resource"
-        fi
-    done
+    resource="module.backup[0].azurerm_recovery_services_vault.vault"
+    if echo "$state_list" | grep -Fxq "$resource"; then
+        terraform state rm "$resource"
+    fi
 fi
 
-cd ..
-echo "Backup-related state cleanup complete."
+cd .. # Not sure if it's required, but go back to the root directory
