12cd0dd13cba1c743a5bc17baf64fff8d8c98661
Ansar Rafique <Ansark6@gmail.com>
2026-07-14 08:05:28 +0100

    Remove additional workspace resources from Terraform state

--- templates/workspaces/base/remove_backup_resources_from_state.sh
+++ templates/workspaces/base/remove_backup_resources_from_state.sh
@@ -2,83 +2,74 @@
 
 set -o errexit
 set -o pipefail
-# Uncomment this line to see each command for debugging (careful: this will show secrets!)
+
+# Uncomment this line to see each command for debugging
 # set -o xtrace
 
 function usage() {
     cat <<USAGE
-    Usage: $0 --backup_enabled true|false --use_azuread_auth true|false --resource_group_name rg_name --storage_account_name sa_name --container_name container --key backend_key
-    Options:
-        --backup_enabled              Whether backup is enabled (true|false)  
-        --use_azuread_auth           Whether to use Azure AD auth (true|false)
-        --use_oidc                   Whether to use OIDC (true|false)
-        --resource_group_name        Backend resource group name
-        --storage_account_name       Backend storage account name  
-        --container_name             Terraform state container name
-        --key                        Backend key for terraform state
+Usage: $0 --backup_enabled true|false --use_azuread_auth true|false --resource_group_name rg_name --storage_account_name sa_name --container_name container --key backend_key
+
+Options:
+    --backup_enabled         Whether backup is enabled (true|false)
+    --use_azuread_auth       Whether to use Azure AD auth (true|false)
+    --resource_group_name    Backend resource group name
+    --storage_account_name   Backend storage account name
+    --container_name         Terraform state container name
+    --key                    Backend key for terraform state
 USAGE
     exit 1
 }
 
-
-# if no arguments are provided, return usage function
 if [ $# -eq 0 ]; then
-    usage # run usage function
+    usage
 fi
 
+backup_enabled=""
+use_azuread_auth=""
+backend_rg_name=""
+backend_storage_account_name=""
+terraform_state_container_name=""
+backend_key=""
 
-
-while [ "$1" != "" ]; do
-    case $1 in
+# Parse Porter flags.
+while [ "$#" -gt 0 ]; do
+    case "$1" in
     --backup_enabled)
-        shift
-        backup_enabled=$1
+        backup_enabled="$2"
+        shift 2
         ;;
     --use_azuread_auth)
-        shift
-        use_azuread_auth=$1
+        use_azuread_auth="$2"
+        shift 2
         ;;
     --resource_group_name)
-        shift
-        backend_rg_name=$1
+        backend_rg_name="$2"
+        shift 2
         ;;
     --storage_account_name)
-        shift
-        backend_storage_account_name=$1
+        backend_storage_account_name="$2"
+        shift 2
         ;;
     --container_name)
-        shift
-        terraform_state_container_name=$1
+        terraform_state_container_name="$2"
+        shift 2
         ;;
     --key)
-        shift
-        backend_key=$1
+        backend_key="$2"
+        shift 2
         ;;
     *)
         echo "Unexpected argument: '$1'"
         usage
         ;;
     esac
-
-    if [[ -z "$2" ]]; then
-      # if no more args then stop processing
-      break
-    fi
-
-    shift # remove the current value for `$1` and use the next
 done
 
-# done with processing args and can set this
 set -o nounset
 
 cd terraform/
 terraform version
-
-echo "${backend_rg_name}"
-echo "${backend_storage_account_name}"
-echo "${terraform_state_container_name}"
-echo "${backend_key}"
-echo "${use_azuread_auth}"
 
 terraform init -input=false -backend=true -reconfigure \
     -backend-config="resource_group_name=${backend_rg_name}" \
@@ -87,27 +78,45 @@ terraform init -input=false -backend=true -reconfigure \
     -backend-config="key=${backend_key}" \
     -backend-config="use_azuread_auth=${use_azuread_auth}"
 
-state_list=$(terraform state list)
+# Read the current state once.
+state_list="$(terraform state list || true)"
 
-resources=(
+# Exact Terraform state addresses for resources we want to unmanage.
+# terraform state rm removes them from state only; it does NOT delete the Azure resources.
+explicit_resources=(
     "azurerm_resource_group.ws"
     "azurerm_storage_account.stg"
-    "azapi_resource.shared_storage"
+    "azurerm_storage_container.stgcontainer"
     "azurerm_storage_account_network_rules.stgrules"
+    "azapi_resource.shared_storage"
     "azurerm_key_vault.kv"
+    "azurerm_private_endpoint.stgfilepe"
+    "azurerm_private_endpoint.stgblobpe"
+    "azurerm_private_endpoint.stgdfspe"
 )
 
-for resource in "${resources[@]}"; do
+echo "Removing core workspace resources from Terraform state..."
+
+for resource in "${explicit_resources[@]}"; do
     if echo "$state_list" | grep -Fxq "$resource"; then
-        terraform state rm "$resource"
+        echo "Removing from state: $resource"
+        terraform state rm "$resource" || true
     fi
 done
 
+# If backup is enabled, remove the whole backup module from state as well.
+# This leaves the backup-related Azure resources orphaned intentionally,
+# so Porter/Terraform can continue uninstalling the rest of the workspace.
 if [[ "${backup_enabled}" == "true" ]]; then
-    resource="module.backup[0].azurerm_recovery_services_vault.vault"
-    if echo "$state_list" | grep -Fxq "$resource"; then
-        terraform state rm "$resource"
-    fi
+    echo "Removing backup module resources from Terraform state..."
+
+    while IFS= read -r resource; do
+        if [ -n "$resource" ]; then
+            echo "Removing from state: $resource"
+            terraform state rm "$resource" || true
+        fi
+    done < <(echo "$state_list" | grep -E '^module\.backup' || true)
 fi
 
-cd .. # Not sure if it's required, but go back to the root directory
+cd ..
+echo "Backup-related state cleanup complete."
